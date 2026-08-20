fe05ab29010dcd1124554407758974e3295667ea
JeevithaS-19 <jeevithajeevithas289@gmail.com>
2026-08-20 15:29:57 +0530

    Day 1: Set up TaskFlow API

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require("dns").setServers(["8.8.8.8"]);
+
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "TaskFlow API is running"
+    });
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`TaskFlow API running on http://localhost:${PORT}`);
+});
